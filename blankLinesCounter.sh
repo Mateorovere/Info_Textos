104609c3abd3bf703af785e7d74e94ea9c35d210
Mateorovere <109367642+Mateorovere@users.noreply.github.com>
2022-08-04 14:17:10 -0300

    Delete blankLinesCounter.sh

--- blankLinesCounter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-total=$( cat $1 | wc -l)
-usadas=$( awk 'NF' $1 | wc -l )
-echo " Cantidad de lineas en blanco: $(( $total - $usadas))"
-# NF: "number of fields" Estas lineas tienen 0 campos, entonces awk relaciona >
