4a8cc0a28128ca057d0ff757f36cd52003bd2d74
Mateorovere <109367642+Mateorovere@users.noreply.github.com>
2022-08-04 14:17:18 -0300

    Delete findNames.sh

--- findNames.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-Nombres="^[A-Z]+[a-z]+[a-z]*"
-PALABRAS=$(cat $1 | tr "? ! : , ; ." "\n")
-for PALABRA in $PALABRAS;
-do
-	if [[ $PALABRA =~ $Nombres ]]; then
-		echo "Nombre:" "$PALABRA"
-	fi
-done
